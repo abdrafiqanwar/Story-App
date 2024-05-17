1f21c48db241623396f348b8d66e73482adac8db
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-05-17 14:57:19 +0800

    fix: story response for adapter

--- app/app/src/main/java/com/example/app/data/response/StoryResponse.kt
+++ app/app/src/main/java/com/example/app/data/response/StoryResponse.kt
@@ -19,20 +19,20 @@ data class ListStoryItem(
 	@field:SerializedName("photoUrl")
 	val photoUrl: String? = null,
 
-	@field:SerializedName("createdAt")
-	val createdAt: String? = null,
-
 	@field:SerializedName("name")
 	val name: String? = null,
 
 	@field:SerializedName("description")
 	val description: String? = null,
 
-	@field:SerializedName("lon")
-	val lon: Double? = null,
+	@field:SerializedName("createdAt")
+	val createdAt: String? = null,
 
 	@field:SerializedName("id")
 	val id: String? = null,
+
+	@field:SerializedName("lon")
+	val lon: Double? = null,
 
 	@field:SerializedName("lat")
 	val lat: Double? = null
