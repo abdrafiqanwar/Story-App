296b457958479626a9dfd82f121b38e48141e7f6
abdrafiqanwar <abd.rafiqanwar@gmail.com>
2024-05-22 13:47:33 +0800

    fix: marker maps

--- app/app/src/main/java/com/example/app/activity/MapsActivity.kt
+++ app/app/src/main/java/com/example/app/activity/MapsActivity.kt
@@ -24,7 +24,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val viewModel by viewModels<MapsViewModel> {
         ViewModelFactory.getInstance(this)
     }
-    private val boundsBuilder = LatLngBounds.Builder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,42 +76,5 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isIndoorLevelPickerEnabled = true
         mMap.uiSettings.isCompassEnabled = true
         mMap.uiSettings.isMapToolbarEnabled = true
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-        addManyMarker()
-    }
-
-    data class TourismPlace(
-        val name: String,
-        val latitude: Double,
-        val longitude: Double
-    )
-
-    private fun addManyMarker() {
-        val tourismPlace = listOf(
-            TourismPlace("Floating Market Lembang", -6.8168954,107.6151046),
-            TourismPlace("The Great Asia Africa", -6.8331128,107.6048483),
-            TourismPlace("Rabbit Town", -6.8668408,107.608081),
-            TourismPlace("Alun-Alun Kota Bandung", -6.9218518,107.6025294),
-            TourismPlace("Orchid Forest Cikole", -6.780725, 107.637409),
-        )
-        tourismPlace.forEach { tourism ->
-            val latLng = LatLng(tourism.latitude, tourism.longitude)
-            mMap.addMarker(MarkerOptions().position(latLng).title(tourism.name))
-            boundsBuilder.include(latLng)
-        }
-
-        val bounds: LatLngBounds = boundsBuilder.build()
-        mMap.animateCamera(
-            CameraUpdateFactory.newLatLngBounds(
-                bounds,
-                resources.displayMetrics.widthPixels,
-                resources.displayMetrics.heightPixels,
-                300
-            )
-        )
     }
 }
